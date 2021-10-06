7136065b4963dfe16c5950048d7d5a8a4536f14a
unknown <micheleghis@gmail.com>
2021-10-06 21:34:39 +0200

    Insert now ok

--- 04_PreparazioneVerifica/insert.sql
+++ 04_PreparazioneVerifica/insert.sql
@@ -1,26 +1,21 @@
-﻿--INSERT INTO Autom VALUES ('FD567LS','BMW',3000,230,1,1);
---INSERT INTO Autom VALUES ('GA334WE','Mercedes',280,200,1,2);
---INSERT INTO Autom VALUES ('EH578OP','Toyota',1600,150,2,2);
---INSERT INTO Autom VALUES ('BZ168KA','Fiat',1200,100,3,3);
+﻿
+INSERT INTO Sinistro(Localita, Data) VALUES ('Torino','20/01/2000');
+INSERT INTO Sinistro(Localita, Data) VALUES ('San Damiano','18/07/2000');
+INSERT INTO Sinistro(Localita, Data) VALUES ('Roma','11/11/2001');
 
---INSERT INTO Proprietari VALUES ('Paolo Damilano', 'Alba');
---INSERT INTO Proprietari VALUES ('Andrea Barbero', 'Fossano');
---INSERT INTO Proprietari VALUES ('Marco Rossi', 'Cuneo');
+INSERT INTO Proprietari(Nome, Residenza) VALUES ('Paolo Damilano', 'Alba');
+INSERT INTO Proprietari(Nome, Residenza) VALUES ('Andrea Barbero', 'Fossano');
+INSERT INTO Proprietari(Nome, Residenza) VALUES ('Marco Rossi', 'Cuneo');
 
---INSERT INTO Assicurazioni VALUES ('SARA', 'Alba');
---INSERT INTO Assicurazioni VALUES ('INASS', 'Torino');
---INSERT INTO Assicurazioni VALUES ('ASSOITA', 'Roma');
+INSERT INTO Assicurazioni(Nome, Sede) VALUES ('SARA', 'Alba');
+INSERT INTO Assicurazioni(Nome, Sede) VALUES ('INASS', 'Torino');
+INSERT INTO Assicurazioni(Nome, Sede) VALUES ('ASSOITA', 'Roma');
 
---INSERT INTO Sinistro VALUES ('Torino','20/01/2000');
---INSERT INTO Sinistro VALUES ('San Damiano','18/07/2000');
---INSERT INTO Sinistro VALUES ('Roma','11/11/2001');
+INSERT INTO Autom VALUES ('FD567LS','BMW',3000,230,1,1);
+INSERT INTO Autom VALUES ('GA334WE','Mercedes',280,200,1,2);
+INSERT INTO Autom VALUES ('EH578OP','Toyota',1600,150,2,2);
+INSERT INTO Autom VALUES ('BZ168KA','Fiat',1200,100,3,3);
 
-INSERT INTO AutoCoinvolte VALUES ('4','BZ168KA',200);
-INSERT INTO AutoCoinvolte VALUES ('7','FD567LS',800);
-INSERT INTO AutoCoinvolte VALUES ('5','EH578OP',90);
-INSERT INTO AutoCoinvolte VALUES ('6','FD567LS',3000);
-
-
---UPDATE Autom SET Marca = 'Seat' WHERE Targa = 'DB456TS'
---UPDATE Sinistro SET Data = '2012-04-22' WHERE CodS = 3
---UPDATE Autocoinvolte SET ImportoDelDanno = 120 WHERE CodS = 3
+INSERT INTO AutoCoinvolte VALUES (2,'GA334WE',200);
+INSERT INTO AutoCoinvolte VALUES (1,'FD567LS',800);
+INSERT INTO AutoCoinvolte VALUES (3,'EH578OP',90);
